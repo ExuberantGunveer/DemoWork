2b6dc0cb0016e618894489c63a10387784e42919
ExuberantGunveer <gunveer.singh@infostride.com>
2022-01-04 18:36:01 +0530

    initial commit

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args) {
 
         int a = 10;
-        int b = 20;
+        int b = 50;
         int c = 30;
 
         int z = ((a>b && a>c)?a:(b>a && b>c)?b:c);
